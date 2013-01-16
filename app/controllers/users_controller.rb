31eb25e29265529fb1dd4393b59f2332b21cdb40
Jason Knebel <jasonkneb@gmail.com>
2013-01-16 17:48:30 -0600

    adding CREATE and DESTORY users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
-  
+
+  def new
+    User.create( name: params[:name], email: params[:email])
+
+    redirect_to root_path
+  end
+
+  def delete
+    User.find(params[:id]).destroy
+    
+    redirect_to root_path
+  end
+
 end
